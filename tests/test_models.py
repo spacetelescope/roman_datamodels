0c2992c254ec53f9978e100242826f8ece532958
perrygreenfield <perry@stsci.edu>
2021-09-09 09:25:46 -0400

    added validation

--- tests/test_models.py
+++ tests/test_models.py
@@ -233,3 +233,14 @@ def test_add_model_attribute(tmp_path):
     assert readnoise.new_attribute == 77
     with pytest.raises(ValueError):
         readnoise['_underscore'] = 'bad'
+
+
+def test_open_with_model_class(tmp_path):
+    # First make test reference file
+    file_path = tmp_path / 'testreadnoise.asdf'
+    utils.mk_readnoise(filepath=file_path)
+    rnmod = datamodels.ReadnoiseRefModel(file_path)
+    assert rnmod.meta.reftype == "READNOISE"
+    assert rnmod.data.shape == (4096, 4224)
+    with pytest.raises(ValueError):
+        wrongmod = datamodels.RampModel(file_path)
