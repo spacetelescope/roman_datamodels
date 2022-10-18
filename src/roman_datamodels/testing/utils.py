3410a8e77df33fa93f7a09ef68ffb19f5aa58d4c
PaulHuwe <phuwe@stsci.edu>
2022-10-18 18:52:33 -0400

    Units updated.

--- src/roman_datamodels/testing/utils.py
+++ src/roman_datamodels/testing/utils.py
@@ -451,7 +451,7 @@ def mk_level1_science_raw(shape=None, filepath=None):
         n_ints = shape[0]
 
     wfi_science_raw['data'] = u.Quantity(np.zeros(shape, dtype=np.uint16),
-                                         u.meter, dtype=np.uint16)
+                                         u.DN, dtype=np.uint16)
 
     # add amp 33 ref pix
     wfi_science_raw['amp33'] = np.zeros((n_ints, 4096, 128), dtype=np.uint16)
@@ -522,12 +522,22 @@ def mk_level2_image(shape=None, n_ints=None, filepath=None):
     amp33_size = (n_ints, 4096, 128)
     wfi_image['amp33'] = np.zeros(amp33_size, dtype=np.uint16)
 
-    wfi_image['data'] = np.zeros(shape, dtype=np.float32)
+    #wfi_image['data'] = np.zeros(shape, dtype=np.float32)
+    wfi_image['data'] = u.Quantity(np.zeros(shape, dtype=np.float32),
+                                         u.electron / u.s, dtype=np.float32)
     wfi_image['dq'] = np.zeros(shape, dtype=np.uint32)
-    wfi_image['err'] = np.zeros(shape, dtype=np.float32)
-    wfi_image['var_poisson'] = np.zeros(shape, dtype=np.float32)
-    wfi_image['var_rnoise'] = np.zeros(shape, dtype=np.float32)
-    wfi_image['var_flat'] = np.zeros(shape, dtype=np.float32)
+    #wfi_image['err'] = np.zeros(shape, dtype=np.float32)
+    wfi_image['err'] = u.Quantity(np.zeros(shape, dtype=np.float32),
+                                         u.electron / u.s, dtype=np.float32)
+    #wfi_image['var_poisson'] = np.zeros(shape, dtype=np.float32)
+    #wfi_image['var_rnoise'] = np.zeros(shape, dtype=np.float32)
+    #wfi_image['var_flat'] = np.zeros(shape, dtype=np.float32)
+    wfi_image['var_poisson'] = u.Quantity(np.zeros(shape, dtype=np.float32),
+                                         u.electron / u.s, dtype=np.float32)
+    wfi_image['var_rnoise'] = u.Quantity(np.zeros(shape, dtype=np.float32),
+                                         u.electron / u.s, dtype=np.float32)
+    wfi_image['var_flat'] = u.Quantity(np.zeros(shape, dtype=np.float32),
+                                         u.electron / u.s, dtype=np.float32)
     wfi_image['cal_logs'] = mk_cal_logs()
 
     if filepath:
@@ -949,13 +959,13 @@ def mk_ramp(shape=None, n_ints=None, filepath=None):
 
     # add border reference pixel arrays
     ramp['border_ref_pix_left'] = u.Quantity(np.zeros((shape[0], shape[1], 4), dtype=np.float32),
-                                                      u.meter, dtype=np.float32)
+                                                      u.DN, dtype=np.float32)
     ramp['border_ref_pix_right'] = u.Quantity(np.zeros((shape[0], shape[1], 4), dtype=np.float32),
-                                                      u.meter, dtype=np.float32)
+                                                      u.DN, dtype=np.float32)
     ramp['border_ref_pix_top'] = u.Quantity(np.zeros((shape[0], 4, shape[2]), dtype=np.float32),
-                                                      u.meter, dtype=np.float32)
+                                                      u.DN, dtype=np.float32)
     ramp['border_ref_pix_bottom'] = u.Quantity(np.zeros((shape[0], 4, shape[2]), dtype=np.float32),
-                                                      u.meter, dtype=np.float32)
+                                                      u.DN, dtype=np.float32)
 
     # and their dq arrays
     ramp['dq_border_ref_pix_left'] = np.zeros((shape[1], 4), dtype=np.uint32)
@@ -967,10 +977,10 @@ def mk_ramp(shape=None, n_ints=None, filepath=None):
     amp33_size = (shape[0], 4096, 128)
     ramp['amp33'] = np.zeros(amp33_size, dtype=np.uint16)
 
-    ramp['data'] = u.Quantity(np.full(shape, 1.0, dtype=np.float32), u.meter, dtype=np.float32)
+    ramp['data'] = u.Quantity(np.full(shape, 1.0, dtype=np.float32), u.DN, dtype=np.float32)
     ramp['pixeldq'] = np.zeros(shape[1:], dtype=np.uint32)
     ramp['groupdq'] = np.zeros(shape, dtype=np.uint8)
-    ramp['err'] = u.Quantity(np.zeros(shape, dtype=np.float32), u.meter, dtype=np.float32)
+    ramp['err'] = u.Quantity(np.zeros(shape, dtype=np.float32), u.DN, dtype=np.float32)
 
     if filepath:
         af = asdf.AsdfFile()
@@ -1007,11 +1017,15 @@ def mk_rampfitoutput(shape=None, filepath=None):
     rampfitoutput['sigslope'] = np.zeros(shape, dtype=np.float32)
     rampfitoutput['yint'] = np.zeros(shape, dtype=np.float32)
     rampfitoutput['sigyint'] = np.zeros(shape, dtype=np.float32)
-    rampfitoutput['pedestal'] = np.zeros(shape[1:], dtype=np.float32)
+    rampfitoutput['pedestal'] = u.Quantity(np.zeros(shape[1:], dtype=np.float32),
+                                           u.electron / u.s, dtype=np.float32)
     rampfitoutput['weights'] = np.zeros(shape, dtype=np.float32)
-    rampfitoutput['crmag'] = np.zeros(shape, dtype=np.float32)
-    rampfitoutput['var_poisson'] = np.zeros(shape, dtype=np.float32)
-    rampfitoutput['var_rnoise'] = np.zeros(shape, dtype=np.float32)
+    rampfitoutput['crmag'] = u.Magnitude(np.zeros(shape, dtype=np.float32) * u.ct/u.s,
+                                         dtype=np.float32)
+    rampfitoutput['var_poisson'] = u.Quantity(np.zeros(shape, dtype=np.float32),
+                                              u.electron / u.s, dtype=np.float32)
+    rampfitoutput['var_rnoise'] = u.Quantity(np.zeros(shape, dtype=np.float32),
+                                             u.electron / u.s, dtype=np.float32)
 
     if filepath:
         af = asdf.AsdfFile()
@@ -1074,10 +1088,20 @@ def mk_guidewindow(shape=None, filepath=None):
     if not shape:
         shape = (2, 8, 16, 32, 32)
 
-    guidewindow['pedestal_frames'] = np.zeros(shape, dtype=np.uint16)
-    guidewindow['signal_frames'] = np.zeros(shape, dtype=np.uint16)
-    guidewindow['amp33'] = np.zeros(shape, dtype=np.uint16)
+    #guidewindow['pedestal_frames'] = np.zeros(shape, dtype=np.uint16)
+    #guidewindow['signal_frames'] = np.zeros(shape, dtype=np.uint16)
+    #guidewindow['amp33'] = np.zeros(shape, dtype=np.uint16)
 
+    guidewindow['pedestal_frames'] = u.Quantity(np.zeros(shape, dtype=np.uint16),
+                                         u.DN, dtype=np.uint16)
+#    guidewindow['pedestal_frames'] = u.Magnitude(np.zeros(shape, dtype=np.uint16) * u.ct/u.s,
+#                                                 dtype=np.uint16)
+    guidewindow['signal_frames'] = u.Quantity(np.zeros(shape, dtype=np.uint16),
+                                         u.DN, dtype=np.uint16)
+    guidewindow['amp33'] = u.Quantity(np.zeros(shape, dtype=np.uint16),
+                                         u.DN, dtype=np.uint16)
+
+    
     if filepath:
         af = asdf.AsdfFile()
         af.tree = {'roman': guidewindow}
