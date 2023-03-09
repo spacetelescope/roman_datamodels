36f004246e4825a1b87c11d1d3d2730e504f4990
William Jamieson <wjamieson@stsci.edu>
2023-03-09 12:51:41 -0500

    Add tests to confirm we write non-vounits correctly

--- tests/test_units.py
+++ tests/test_units.py
@@ -14,7 +14,7 @@ def test_units(unit):
 
 @pytest.mark.parametrize("unit", units.__all__)
 def test_base_roman_unit(unit):
-    """Test deserialization of a base Roman units"""
+    """Test deserialization of a base legacy Roman units"""
     yaml = f"""
 unit: !<asdf://stsci.edu/datamodels/roman/tags/unit-1.0.0> {unit}
     """
@@ -34,3 +34,20 @@ unit: !<asdf://stsci.edu/datamodels/roman/tags/unit-1.0.0> {unit}
     buff = yaml_to_asdf(yaml)
     with asdf.open(buff) as af:
         assert af["unit"] == u.Unit(unit, parse_strict="silent")
+
+
+@pytest.mark.parametrize("unit", [u.electron, u.DN, u.electron / u.s, u.electron**2 / u.s**2, u.electron / u.DN])
+def test_non_vounit(unit, tmp_path):
+    """Test deserialization of a non-VOUnits used by Roman"""
+
+    file_path = tmp_path / "test.asdf"
+    with asdf.AsdfFile() as af:
+        af["unit"] = unit
+        af.write_to(file_path)
+
+    with asdf.open(file_path) as af:
+        assert af["unit"] == unit
+
+    with asdf.open(file_path, _force_raw_types=True) as af:
+        assert isinstance(af["unit"], asdf.tagged.TaggedString)
+        assert af["unit"]._tag.startswith("tag:astropy.org:astropy/units/unit-")
