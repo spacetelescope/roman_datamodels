8a86df71368c3d9f2468e2e7993c82221b8a5141
Zach Burnett <zburnett@stsci.edu>
2023-04-24 08:19:41 -0400

    use fix from GitHub discussion for Python 3.9

--- docs/conf.py
+++ docs/conf.py
@@ -26,13 +26,15 @@ else:
     import tomllib
 
 
+def find_mod_objs_patched(*args, **kwargs):
+    return find_mod_objs(args[0], onlylocals=True)
+
 def patch_automodapi(app):
     """Monkey-patch the automodapi extension to exclude imported members"""
     from sphinx_automodapi import automodsumm
     from sphinx_automodapi.utils import find_mod_objs
 
-    automodsumm.find_mod_objs = lambda *args: find_mod_objs(args[0], onlylocals=True)
-
+    automodsumm.find_mod_objs = find_mod_objs_patched
 
 def setup(app):
     try:
