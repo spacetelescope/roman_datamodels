4901da68183c25bae9cb49bf24d9874fa43eab06
William Jamieson <wjamieson@stsci.edu>
2023-06-02 10:33:42 -0400

    Fix the `reftype` change from `INVERSE_LINEARITY` to `INVERSE-LINEARITY`.

--- src/roman_datamodels/testing/factories.py
+++ src/roman_datamodels/testing/factories.py
@@ -418,7 +418,7 @@ def create_inverse_linearity_ref(**kwargs):
     raw = {
         "coeffs": random_utils.generate_array_float32((2, 4096, 4096)),
         "dq": random_utils.generate_array_uint32((4096, 4096)),
-        "meta": create_ref_meta(reftype="INVERSE_LINEARITY"),
+        "meta": create_ref_meta(reftype="INVERSE-LINEARITY"),
     }
     raw.update(kwargs)
 
