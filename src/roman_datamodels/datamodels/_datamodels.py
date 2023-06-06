aa1c8adb5db70469b654933a8395e6219a14a9f0
William Jamieson <wjamieson@stsci.edu>
2023-06-06 17:01:17 -0400

    Move `open` to `_utils` as `rdm_open`

--- src/roman_datamodels/datamodels/_datamodels.py
+++ src/roman_datamodels/datamodels/_datamodels.py
@@ -7,28 +7,11 @@ keep consistency with the JWST data model version.
 It is to be subclassed by the various types of data model variants for products
 """
 
-import warnings
-
-import asdf
 import numpy as np
-import packaging.version
 
-from roman_datamodels import stnode, validate
+from roman_datamodels import stnode
 
-from ._core import MODEL_REGISTRY, DataModel
-
-# .dev is included in the version comparison to allow for correct version
-# comparisons with development versions of asdf 3.0
-if packaging.version.Version(asdf.__version__) < packaging.version.Version("3.dev"):
-    with warnings.catch_warnings():
-        warnings.filterwarnings(
-            "ignore",
-            category=asdf.exceptions.AsdfDeprecationWarning,
-            message=r"AsdfInFits has been deprecated.*",
-        )
-        from asdf.fits_embed import AsdfInFits
-else:
-    AsdfInFits = None
+from ._core import DataModel
 
 __all__ = [
     "ImageModel",
@@ -52,7 +35,6 @@ __all__ = [
     "IpcRefModel",
     "InverseLinearityRefModel",
     "MosaicModel",
-    "open",
 ]
 
 
@@ -211,71 +193,3 @@ class WfiImgPhotomRefModel(DataModel):
 
 class RefpixRefModel(DataModel):
     _node_type = stnode.RefpixRef
-
-
-def open(init, memmap=False, target=None, **kwargs):
-    """
-    Data model factory function
-
-    Parameters
-    ----------
-    init : str, `DataModel`, `asdf.AsdfFile`
-        May be any one of the following types:
-            - `asdf.AsdfFile` instance
-            - string indicating the path to an ASDF file
-            - `DataModel` Roman data model instance
-    memmap : bool
-        Open ASDF file binary data using memmap (default: False)
-    target : `DataModel`
-        If not None value, the `DataModel` implied by the init argument
-        must be an instance of the target class. If the init value
-        is already a data model, and matches the target, the init
-        value is returned, not copied, as opposed to the case where
-        the init value is a data model, and target is not supplied,
-        and the returned value is a copy of the init value.
-
-    Returns
-    -------
-    `DataModel`
-    """
-    with validate.nuke_validation():
-        file_to_close = None
-        if target is not None:
-            if not issubclass(target, DataModel):
-                raise ValueError("Target must be a subclass of DataModel")
-        # Temp fix to catch JWST args before being passed to asdf open
-        if "asn_n_members" in kwargs:
-            del kwargs["asn_n_members"]
-        if isinstance(init, asdf.AsdfFile):
-            asdffile = init
-        elif isinstance(init, DataModel):
-            if target is not None:
-                if not isinstance(init, target):
-                    raise ValueError("First argument is not an instance of target")
-                else:
-                    return init
-            # Copy the object so it knows not to close here
-            return init.copy()
-        else:
-            try:
-                kwargs["copy_arrays"] = not memmap
-                asdffile = asdf.open(init, **kwargs)
-                file_to_close = asdffile
-            except ValueError:
-                raise TypeError("Open requires a filepath, file-like object, or Roman datamodel")
-            if AsdfInFits is not None and isinstance(asdffile, AsdfInFits):
-                if file_to_close is not None:
-                    file_to_close.close()
-                raise TypeError("Roman datamodels does not accept FITS files or objects")
-        modeltype = type(asdffile.tree["roman"])
-        if modeltype in MODEL_REGISTRY:
-            rmodel = MODEL_REGISTRY[modeltype](asdffile, **kwargs)
-            if target is not None:
-                if not issubclass(rmodel.__class__, target):
-                    if file_to_close is not None:
-                        file_to_close.close()
-                    raise ValueError("Referenced ASDF file model type is not subclass of target")
-            else:
-                return rmodel
-        else:
-            return DataModel(asdffile, **kwargs)
