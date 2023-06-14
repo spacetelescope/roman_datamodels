d312f7a42c9e1f9968b79a9ce700e7b0d237821a
William Jamieson <wjamieson@stsci.edu>
2023-06-14 14:38:05 -0400

    Add default overrides for `_basic_meta` makers

--- src/roman_datamodels/maker_utils/_basic_meta.py
+++ src/roman_datamodels/maker_utils/_basic_meta.py
@@ -5,7 +5,7 @@ from roman_datamodels import stnode
 from ._base import NOSTR
 
 
-def mk_calibration_software_version():
+def mk_calibration_software_version(**kwargs):
     """
     Create a dummy CalibrationSoftwareVersion object with valid values
 
@@ -13,10 +13,10 @@ def mk_calibration_software_version():
     -------
     roman_datamodels.stnode.CalibrationSoftwareVersion
     """
-    return stnode.CalibrationSoftwareVersion("9.9.0")
+    return stnode.CalibrationSoftwareVersion(kwargs.get("calibration_software_version", "9.9.0"))
 
 
-def mk_sdf_software_version():
+def mk_sdf_software_version(**kwargs):
     """
     Create a dummy SdfSoftwareVersion object with valid values
 
@@ -25,10 +25,10 @@ def mk_sdf_software_version():
     roman_datamodels.stnode.SdfSoftwareVersion
     """
 
-    return stnode.SdfSoftwareVersion("7.7.7")
+    return stnode.SdfSoftwareVersion(kwargs.get("sdf_software_version", "7.7.7"))
 
 
-def mk_filename():
+def mk_filename(**kwargs):
     """
     Create a dummy Filename object with valid values
 
@@ -36,10 +36,10 @@ def mk_filename():
     -------
     roman_datamodels.stnode.Filename
     """
-    return stnode.Filename(NOSTR)
+    return stnode.Filename(kwargs.get("filename", NOSTR))
 
 
-def mk_file_date():
+def mk_file_date(**kwargs):
     """
     Create a dummy FileDate object with valid values
 
@@ -48,10 +48,10 @@ def mk_file_date():
     roman_datamodels.stnode.FileDate
     """
 
-    return stnode.FileDate(time.Time("2020-01-01T00:00:00.0", format="isot", scale="utc"))
+    return stnode.FileDate(kwargs.get("file_date", time.Time("2020-01-01T00:00:00.0", format="isot", scale="utc")))
 
 
-def mk_model_type():
+def mk_model_type(**kwargs):
     """
     Create a dummy ModelType object with valid values
 
@@ -59,10 +59,10 @@ def mk_model_type():
     -------
     roman_datamodels.stnode.ModelType
     """
-    return stnode.ModelType(NOSTR)
+    return stnode.ModelType(kwargs.get("model_type", NOSTR))
 
 
-def mk_origin():
+def mk_origin(**kwargs):
     """
     Create a dummy Origin object with valid values
 
@@ -71,10 +71,10 @@ def mk_origin():
     roman_datamodels.stnode.Origin
     """
 
-    return stnode.Origin("STSCI")
+    return stnode.Origin(kwargs.get("origin", "STSCI"))
 
 
-def mk_prd_software_version():
+def mk_prd_software_version(**kwargs):
     """
     Create a dummy PrdSoftwareVersion object with valid values
 
@@ -82,10 +82,10 @@ def mk_prd_software_version():
     -------
     roman_datamodels.stnode.PrdSoftwareVersion
     """
-    return stnode.PrdSoftwareVersion("8.8.8")
+    return stnode.PrdSoftwareVersion(kwargs.get("prd_software_version", "8.8.8"))
 
 
-def mk_telescope():
+def mk_telescope(**kwargs):
     """
     Create a dummy Telescope object with valid values
 
@@ -93,26 +93,25 @@ def mk_telescope():
     -------
     roman_datamodels.stnode.Telescope
     """
-    return stnode.Telescope("ROMAN")
+    return stnode.Telescope(kwargs.get("telescope", "ROMAN"))
 
 
-def mk_basic_meta():
+def mk_basic_meta(**kwargs):
     """
     Create a dummy basic metadata dictionary with valid values for attributes
-        TODO: This needs to be updated with constructors for the scalar objects
 
     Returns
     -------
     dict (defined by the basic-1.0.0 schema)
     """
     meta = {}
-    meta["calibration_software_version"] = mk_calibration_software_version()
-    meta["sdf_software_version"] = mk_sdf_software_version()
-    meta["filename"] = mk_filename()
-    meta["file_date"] = mk_file_date()
-    meta["model_type"] = mk_model_type()
-    meta["origin"] = mk_origin()
-    meta["prd_software_version"] = mk_prd_software_version()
-    meta["telescope"] = mk_telescope()
+    meta["calibration_software_version"] = mk_calibration_software_version(**kwargs)
+    meta["sdf_software_version"] = mk_sdf_software_version(**kwargs)
+    meta["filename"] = mk_filename(**kwargs)
+    meta["file_date"] = mk_file_date(**kwargs)
+    meta["model_type"] = mk_model_type(**kwargs)
+    meta["origin"] = mk_origin(**kwargs)
+    meta["prd_software_version"] = mk_prd_software_version(**kwargs)
+    meta["telescope"] = mk_telescope(**kwargs)
 
     return meta
