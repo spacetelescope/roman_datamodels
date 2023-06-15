03909b693360751c471ebf126baebe7bb417401f
William Jamieson <wjamieson@stsci.edu>
2023-06-15 11:46:27 -0400

    Add unit tests for ability to override

--- tests/test_maker_utils.py
+++ tests/test_maker_utils.py
@@ -1,8 +1,13 @@
+from unittest import mock
+
 import asdf
 import pytest
+from astropy import units as u
+from astropy.time import Time
 
 from roman_datamodels import datamodels, maker_utils, stnode
 from roman_datamodels.maker_utils import _ref_files as ref_files
+from roman_datamodels.testing import assert_node_equal
 
 
 @pytest.mark.parametrize("node_class", stnode.NODE_CLASSES)
@@ -55,8 +60,8 @@ def test_ref_files_all(name):
     assert method_name[:-4] in ref_files.__all__
 
 
-@pytest.mark.parametrize("util", [c.__name__ for c in datamodels.MODEL_REGISTRY])
-def test_make_datamodel_tests(util):
+@pytest.mark.parametrize("node_class", [node for node in datamodels.MODEL_REGISTRY])
+def test_make_datamodel_tests(node_class):
     """
     Meta test to confirm that correct tests exist for each datamodel maker utility.
     """
@@ -64,7 +69,8 @@ def test_make_datamodel_tests(util):
 
     from . import test_models as tests
 
-    name = maker_utils.SPECIAL_MAKERS.get(util, _camel_case_to_snake_case(util))
+    name = node_class.__name__
+    name = maker_utils.SPECIAL_MAKERS.get(name, _camel_case_to_snake_case(name))
     if name.startswith("mk_"):
         name = name[3:]
     if name.endswith("_ref"):
@@ -94,3 +100,72 @@ def test_datamodel_maker(model_class):
 
     assert isinstance(model, model_class)
     model.validate()
+
+
+@pytest.mark.parametrize("node_class", [node for node in datamodels.MODEL_REGISTRY])
+def test_override_data(node_class):
+    """
+    Test that we can override data in any maker, all makers are part included in some datamodel,
+    so it is sufficient to work over just the datamodel node classes.
+
+    Note:
+        This test is a bit involved because we need to figure out all the possible overrides on the
+        fly. The way we do this is to run the maker utility, then walk the resulting node object mutating
+        everything along the way. We record the nested dictionary of mutated values and then pass that
+        to the maker utility as the override data.
+
+        Note that we use MagicMock objects to create mutated data, this is because they are very
+        simple to generate new distinct instances that are easy to check. However, they will not
+        pass validation. Because of the way all the maker utilities are implemented, they do not
+        themselves validate the data as they make the object, so this is fine. The above tests
+        explicitly validate all the maker utility outputs, so we don't need to worry about it here.
+        The purpose here is just to inject unique non-default data into the maker utility to override
+        its default values.
+    """
+
+    def mutate_value(value):
+        """
+        Generate a mutated value for a given value.
+            Note:
+                - Time is a special case because it's constructor is picky.
+                - Pure lists need to be preserved.
+                - TaggedScalarNodes need their type preserved.
+        """
+        if isinstance(value, Time):
+            return value + 1 * u.day
+
+        if isinstance(value, list):
+            return [mock.MagicMock()]
+
+        if isinstance(value, stnode.TaggedScalarNode):
+            return value.__class__(mutate_value(value.__class__.__bases__[0](value)))
+
+        return mock.MagicMock()
+
+    def mutate_node(node):
+        """
+        Walk the node object and mutate all its values, returning a dict of the mutated values.
+        """
+        if isinstance(node, stnode.DNode):
+            dict_ = {}
+            for key in node:
+                value = mutate_node(getattr(node, key))
+                node[key] = value
+                dict_[key] = value
+
+            return node.__class__(dict_)
+
+        elif isinstance(node, stnode.LNode):
+            return node.__class__([mutate_node(value) for value in node])
+
+        else:
+            return mutate_value(node)
+
+    # Create a node then mutate it.
+    node = maker_utils.mk_node(node_class)
+    kwargs = mutate_node(node)
+
+    # Create a new node using the recorded mutation data. Then check it is equal to the mutated object.
+    new_node = maker_utils.mk_node(node_class, **kwargs)
+    assert new_node is not node
+    assert_node_equal(new_node, node)
