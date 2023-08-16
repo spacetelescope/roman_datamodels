a967c3fd00f0421748fcb96558e650bba452e422
William Jamieson <wjamieson@stsci.edu>
2023-08-16 11:19:58 -0400

    Eradicate commented out code
    Commented out code will still exist in the version control system so there is no reason to persist it

--- src/roman_datamodels/table_definitions.py
+++ src/roman_datamodels/table_definitions.py
@@ -1,8 +1,10 @@
-# Pleaceholder for future tables, ex:
-#
-# DQ_DEF_DTYPE = np.dtype([
-#     ("BIT", np.int32),
-#     ("VALUE", np.uint32),
-#     ("NAME", "S40"),
-#     ("DESCRIPTION", "S80")
-# ])
+"""
+Pleaceholder for future tables, ex:
+
+DQ_DEF_DTYPE = np.dtype([
+    ("BIT", np.int32),
+    ("VALUE", np.uint32),
+    ("NAME", "S40"),
+    ("DESCRIPTION", "S80")
+])
+"""
