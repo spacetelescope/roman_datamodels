012e02a5734ddd442c15b121c909e0c0b9c79461
William Jamieson <wjamieson@stsci.edu>
2024-01-05 13:01:39 -0500

    Added to_asdf and its tests

--- src/roman_datamodels/datamodels/datamodel.py
+++ src/roman_datamodels/datamodels/datamodel.py
@@ -6,6 +6,7 @@ from __future__ import annotations
 
 import abc
 import warnings
+from contextlib import contextmanager
 from pathlib import Path
 
 import asdf
@@ -24,24 +25,112 @@ class RomanDataModel(BaseRomanDataModel):
     _asdf: asdf.AsdfFile | None = None
     _asdf_external: bool = False
 
+    @contextmanager
+    def _temporary_filename_update(self, filename: str) -> None:
+        """
+        Context manager to temporally update the meta.filename attribute (if it exists), then
+        restore it to its original value after the context exits.
+            This is to facilitate writing the resulting model to a new ASDF file while enabling
+            the filename in the newly written file to make sense, while retaining the original filename
+            for the model in memory.
+        """
+        if hasattr(self, "meta") and hasattr(self.meta, "filename"):
+            old_filename = self.meta.filename
+            self.meta.filename = filename
+            yield
+            self.meta.filename = old_filename
+        else:
+            yield
+
+        return
+
+    @staticmethod
+    def open_asdf(file: asdf_file = None, **kwargs) -> asdf.AsdfFile:
+        """
+        Attempt to open an ASDF file.
+
+        Parameters
+        ----------
+        file: str, Path, asdf.AsdfFile, or None
+            The "file" to open, default is None.
+            - If None, a new ASDF file is created.
+            - If str or Path, the file is opened using asdf.open().
+            - If asdf.AsdfFile, the object is still passed through to asdf.AsdfFile(), but that
+              should just return the same object back.
+        **kwargs :
+            Additional keyword arguments passed to asdf.open() or asdf.AsdfFile().
+
+        Returns
+        -------
+        An asdf.AsdfFile object.
+        """
+        if not isinstance(file, asdf_file):
+            raise TypeError(f"Expected file to be a string, Path, asdf.AsdfFile, or None; not {type(file).__name__}")
+
+        return asdf.open(file, **kwargs) if isinstance(file, (str, Path)) else asdf.AsdfFile(file, **kwargs)
+
+    def to_asdf(self, file: str | Path, *args, **kwargs) -> asdf.AsdfFile:
+        """
+        Write the model to an ASDF file.
+        """
+        if not isinstance(file, (str, Path)):
+            raise TypeError(f"Expected file to be a string or Path; not {type(file).__name__}")
+
+        with self._temporary_filename_update(Path(file).name):
+            # Open a blank ASDF file to write model to (note file not passed to open_asdf())
+            af = self.open_asdf(**kwargs)
+            af.tree = {"roman": self}
+            af.write_to(file, *args, **kwargs)
+
+            return af
+
     @classmethod
     def from_asdf(cls, file: asdf_file = None) -> RomanDataModel:
-        if not isinstance(file, asdf_file):
-            raise TypeError("Expected file to be a string, Path, asdf.AsdfFile, or None")
+        """
+        Read a RomanDataModel from an ASDF file.
 
+        Parameters
+        ----------
+        file : str, Path, asdf.AsdfFile, or None
+            The ASDF file to read from.
+            - If None, a new model is created using the default values.
+            - If str or Path, the file is opened using asdf.open(). Note the model
+              will attempt to close the file when it is "closed".
+            - If asdf.AsdfFile, that file is assumed to contain the model. Note
+              that if a file is directly passed in, the resulting model will not
+              attempt to close the file when it is closed.
+
+        Returns
+        -------
+        A RomanDataModel from the ASDF file.
+        """
+        if not isinstance(file, asdf_file):
+            raise TypeError(f"Expected file to be a string, Path, asdf.AsdfFile, or None; not {type(file).__name__}")
+
+        # Handle closing the file if/when necessary
         opened_file = False
         external_asdf = True
 
         if file is None:
+            # Create a new model with the default values
             new_cls = cls.make_default()
             warnings.warn("No file provided, creating default model")
         else:
+            # Attempt to open a file if necessary
             if isinstance(file, (str, Path)):
                 file = asdf.open(file)
                 opened_file = True
                 external_asdf = False
 
-            new_cls = file.tree["roman"]
+            # Attempt to grab the model from the file.
+            #    Note that roman_datamodels always assumes that the model is stored
+            #    under the "roman" keyword branching off the asdf tree root.
+            try:
+                new_cls = file.tree["roman"]
+            except KeyError as err:
+                if opened_file:
+                    file.close()
+                raise KeyError(f"{cls.__name__}.from_asdf expects a file with a 'roman' key") from err
 
         # Check that the model is of the correct type and return it
         if isinstance(new_cls, cls):
@@ -54,17 +143,6 @@ class RomanDataModel(BaseRomanDataModel):
             file.close()
 
         raise TypeError(f"Expected file containing model of type {cls.__name__}, got {type(new_cls).__name__}")
-
-    # def to_asdf(self, file: asdf_file = None) -> asdf.AsdfFile:
-    #     if file is None:
-    #         file = asdf.AsdfFile()
-
-    #     if isinstance(file, (str, Path)):
-    #         file = asdf.AsdfFile.open(file)
-
-    #     # Set the model in the ASDF file and return it
-    #     file.tree["roman"] = self
-    #     return file
 
     def close(self):
         if not (self._asdf_external or self._asdf is None):
--- src/roman_datamodels/pydantic/tests/test_asdf.py
+++ src/roman_datamodels/pydantic/tests/test_asdf.py
@@ -1,6 +1,9 @@
 """
 Run tests on writing the data models to ASDF
 """
+from pathlib import Path
+from unittest import mock
+
 import asdf
 import pytest
 
@@ -14,22 +17,182 @@ models = [
 ]
 
 
-# TODO: change to using .to_asdf() later
 @pytest.mark.parametrize("model", models)
-def test_write_asdf(model, tmp_path):
+class TestOpenAsdf:
     """
-    Test that reading and writing the model to ASDF doesn't raise an error.
-        This is only a face value check, not an exactness check.
+    Test the open_asdf method
     """
 
-    filename = tmp_path / "test.asdf"
+    def create_model(self, model, tmp_path):
+        """
+        Instantiate the model and create a file path to write it to
+        """
+        filename = tmp_path / "test.asdf"
+        asdf.AsdfFile({"foo": "bar"}).write_to(filename)
 
-    # _shrink=True is used to limit testing memory use
-    af = asdf.AsdfFile({"roman_datamodel": model.make_default(_shrink=True)})
-    af.write_to(filename)
+        return model.make_default(_shrink=True), filename
 
-    with asdf.open(filename) as af:
-        assert isinstance(af["roman_datamodel"], model)
+    def test_str(self, model, tmp_path):
+        """
+        Test that we can open an ASDF file represented by a str
+        """
+        instance, filename = self.create_model(model, tmp_path)
+
+        # Open the ASDF file
+        af = instance.open_asdf(str(filename))
+        assert isinstance(af, asdf.AsdfFile)
+        assert af.tree["foo"] == "bar"
+
+        # Have to close this because open_asdf doesn't close it on purpose
+        af.close()
+
+    def test_path(self, model, tmp_path):
+        """
+        Test that we can open an ASDF file represented by a path
+        """
+        instance, filename = self.create_model(model, tmp_path)
+        assert isinstance(filename, Path)
+
+        # Open the ASDF file
+        af = instance.open_asdf(filename)
+        assert isinstance(af, asdf.AsdfFile)
+        assert af.tree["foo"] == "bar"
+
+        # Have to close this because open_asdf doesn't close it on purpose
+        af.close()
+
+    def test_asdf_file(self, model, tmp_path):
+        """
+        Test that we can open an existing ASDF file
+        """
+        instance, filename = self.create_model(model, tmp_path)
+
+        # Open the ASDF file with asdf
+        with asdf.open(filename) as af:
+            # Now open with open_asdf
+            new_af = instance.open_asdf(af)
+
+            assert isinstance(new_af, asdf.AsdfFile)
+            assert new_af.tree["foo"] == "bar"
+
+    def test_none(self, model, tmp_path):
+        """
+        Test that we can open nothing
+        """
+        instance, filename = self.create_model(model, tmp_path)
+
+        # Open nothing
+        af = instance.open_asdf()
+        assert isinstance(af, asdf.AsdfFile)
+        assert af.tree == {}
+
+
+@pytest.mark.parametrize("model", models)
+class TestToAsdf:
+    """
+    Test the to_asdf method
+    """
+
+    def create_model(self, model, tmp_path):
+        """
+        Instantiate the model and create a file path to write it to
+        """
+        return model.make_default(_shrink=True), tmp_path / "test.asdf"
+
+    def test_str(self, model, tmp_path):
+        """
+        Test writing a model to a file represented by a str
+        """
+        instance, filename = self.create_model(model, tmp_path)
+
+        # Write to ASDF
+        #     This is also testing the asdf converter can serialize the model
+        af = instance.to_asdf(str(filename))
+
+        # Check that the asdf file created contains the model
+        assert isinstance(af, asdf.AsdfFile)
+        assert af.tree["roman"] is instance
+
+        # Check that the written file is then readable
+        #    Note .from_asdf is not used so that issues with .to_asdf can be debugged
+        #    independently using the tests.
+        # This also check the asdf converter can deserialize to the model
+        #    This is only a face value check, not a round trip check.
+        with asdf.open(filename) as af:
+            assert isinstance(af["roman"], model)
+
+    def test_path(self, model, tmp_path):
+        """
+        Test writing a model to a file represented by a str
+        """
+        instance, filename = self.create_model(model, tmp_path)
+        assert isinstance(filename, Path)
+
+        # Write to ASDF
+        #     This is also testing the asdf converter can serialize the model
+        af = instance.to_asdf(filename)
+
+        # Check that the asdf file created contains the model
+        assert isinstance(af, asdf.AsdfFile)
+        assert af.tree["roman"] is instance
+
+        # Check that the written file is then readable
+        #    Note .from_asdf is not used so that issues with .to_asdf can be debugged
+        #    independently using the tests.
+        # This also check the asdf converter can deserialize to the model
+        #    This is only a face value check, not a round trip check.
+        with asdf.open(filename) as af:
+            assert isinstance(af["roman"], model)
+
+    def test_other_input(self, model):
+        """
+        Test that we raise an error if the input is not a string, Path, or asdf.AsdfFile
+        """
+        with pytest.raises(TypeError, match=r"Expected file to be a string or Path; not.*"):
+            model.make_default(_shrink=True).to_asdf(mock.MagicMock())
+
+    def test_includes_extras(self, model, tmp_path):
+        """
+        Test that we can add, write, and read-back extra data to a model.
+        """
+        instance, filename = self.create_model(model, tmp_path)
+
+        # Add extra field and check that it is there
+        #    Note this shows the `extra="allow"` configuration option is enabled
+        instance.foo = "bar"
+        assert instance.foo == "bar"
+
+        # Write to ASDF
+        instance.to_asdf(filename)
+
+        # Read back from ASDF contains the extra field
+        #    Note this is a partial round trip check, as it shows that at least one
+        #    field (the extra one) is round tripped.
+        with asdf.open(filename) as af:
+            assert isinstance(af["roman"], model)
+            assert af["roman"].foo == "bar"
+
+    def test_proper_filename(self, model, tmp_path):
+        """
+        Test that the filename in the meta is updated when writing to a file
+        """
+        instance, filename = self.create_model(model, tmp_path)
+        if hasattr(instance, "meta") and hasattr(instance.meta, "filename"):
+            assert instance.meta.filename != filename
+
+            current_filename = instance.meta.filename
+
+            # Write to ASDF
+            instance.to_asdf(filename)
+
+            # Check that the filename in the meta is not updated
+            assert instance.meta.filename == current_filename
+
+            # Read back from ASDF and check that the filename in the meta is updated
+            #    to reflect the name of the file that it was written to
+            with asdf.open(filename) as af:
+                assert isinstance(af["roman"], model)
+                assert af["roman"].meta.filename == filename.name
 
 
 def test_observation(tmp_path):
@@ -39,43 +202,15 @@ def test_observation(tmp_path):
     """
 
     filename = tmp_path / "test.asdf"
-
-    # _shrink=True is used to limit testing memory use
-    af = asdf.AsdfFile({"roman_datamodel": _generated.Observation.make_default(_shrink=True)})
-    af.write_to(filename)
+    model = _generated.Observation.make_default(_shrink=True)
+    model.to_asdf(filename)
 
     # Force raw types so we can look at the ASDF tree itself
     with asdf.open(filename, _force_raw_types=True) as af:
-        assert "pass" in af.tree["roman_datamodel"]
-        assert "pass_" not in af.tree["roman_datamodel"]
-
-        assert af.tree["roman_datamodel"]["pass"] == -999999
-
-
-@pytest.mark.parametrize("model", models)
-def test_extras(model, tmp_path):
-    """
-    Test that we can add, write, and read-back extra data to a model.
-    """
-
-    filename = tmp_path / "test.asdf"
-
-    # _shrink=True is used to limit testing memory use
-    instance = model.make_default(_shrink=True)
-
-    # Add extra field and check that it is there
-    instance.foo = "bar"
-    assert instance.foo == "bar"
-
-    # Write to ASDF
-    af = asdf.AsdfFile({"roman_datamodel": instance})
-    af.write_to(filename)
-
-    # Read back from ASDF contains the extra field
-    with asdf.open(filename) as af:
-        assert isinstance(af["roman_datamodel"], model)
-        new_instance = af["roman_datamodel"]
-        assert new_instance.foo == "bar"
+        # Check that the data is correctly written under pass not pass
+        assert "pass" in af.tree["roman"]
+        assert "pass_" not in af.tree["roman"]
+        assert af.tree["roman"]["pass"] == -999999 == model.pass_
 
 
 @pytest.mark.parametrize("model", models)
@@ -87,17 +222,13 @@ class TestFromAsdf:
     def create_asdf(self, model, tmp_path):
         """Create an ASDF file with the model in it"""
         filename = tmp_path / "test.asdf"
-
-        # Write to ASDF
-        af = asdf.AsdfFile({"roman": model.make_default(_shrink=True)})  # TODO: change to using .to_asdf() later
-        af.write_to(filename)
-        af.close()
+        model.make_default(_shrink=True).to_asdf(filename)
 
         return filename
 
-    def test_file_name_str(self, model, tmp_path):
+    def test_str(self, model, tmp_path):
         """
-        Test that we can read a model from a file name
+        Test that we can read a model from a file represented by a str
         """
         filename = self.create_asdf(model, tmp_path)
 
@@ -115,11 +246,12 @@ class TestFromAsdf:
             assert instance._asdf is not None
             assert isinstance(instance._asdf, asdf.AsdfFile)
 
-    def test_file_name_path(self, model, tmp_path):
+    def test_path(self, model, tmp_path):
         """
-        Test that we can read a model from a file name
+        Test that we can read a model from a file represented by a path
         """
         filename = self.create_asdf(model, tmp_path)
+        assert isinstance(filename, Path)
 
         # Read back using RomanDataModel.from_asdf
         with RomanDataModel.from_asdf(filename) as instance:
@@ -137,7 +269,7 @@ class TestFromAsdf:
 
     def test_asdf_file(self, model, tmp_path):
         """
-        Test that we can read a model from an ASDF file
+        Test that we can read a model from an existing ASDF file
         """
         filename = self.create_asdf(model, tmp_path)
 
@@ -176,12 +308,12 @@ class TestFromAsdf:
         """
 
         # Using RomanDataModel.from_asdf
-        with pytest.raises(TypeError, match=r"Expected file to be a string, Path, asdf.AsdfFile, or None"):
-            RomanDataModel.from_asdf(42)
+        with pytest.raises(TypeError, match=r"Expected file to be a string, Path, asdf.AsdfFile, or None; not.*"):
+            RomanDataModel.from_asdf(mock.MagicMock())
 
         # Using model.from_asdf
-        with pytest.raises(TypeError, match=r"Expected file to be a string, Path, asdf.AsdfFile, or None"):
-            model.from_asdf(42)
+        with pytest.raises(TypeError, match=r"Expected file to be a string, Path, asdf.AsdfFile, or None; not.*"):
+            model.from_asdf(mock.MagicMock())
 
     def test_wrong_model_type(self, model, tmp_path):
         """
@@ -196,3 +328,49 @@ class TestFromAsdf:
         # Try to open the file with the wrong model class
         with pytest.raises(TypeError, match=r"Expected file containing model of type.*, got.*"):
             other_model.from_asdf(filename)
+
+    def create_dummy_asdf(self, tmp_path):
+        """
+        Create a dummy ASDF file (not containing a RomanDataModel)
+        """
+        # Create a file with a non-data model in it
+        filename = tmp_path / "test.asdf"
+        asdf.AsdfFile({"foo": "bar"}).write_to(filename)
+
+        return filename
+
+    def test_open_non_data_model_file(self, model, tmp_path):
+        """
+        Test that we catch if the file doesn't contain a data model and close the model.
+            This test assumes we pass a str/Path to RomanDataModel.from_asdf (not an open asdf.AsdfFile)
+            pytest will raise a file not closed error if we don't close the file.
+        """
+        filename = self.create_dummy_asdf(tmp_path)
+
+        # Try to open the file with RomanDataModel.from_asdf
+        with pytest.raises(KeyError, match=r".*from_asdf expects a file with a 'roman' key"):
+            RomanDataModel.from_asdf(filename)
+
+        # Try to open the file with model.from_asdf
+        with pytest.raises(KeyError, match=r".*from_asdf expects a file with a 'roman' key"):
+            model.from_asdf(filename)
+
+    def test_open_non_data_model_asdffile(self, model, tmp_path):
+        """
+        Test that we catch if the file doesn't contain a data model and close the model.
+            This test assumes we pass an open asdf.AsdfFile to RomanDataModel.from_asdf.
+        """
+        filename = self.create_dummy_asdf(tmp_path)
+
+        with asdf.open(filename) as af:
+            # Try to open the file with RomanDataModel.from_asdf
+            with pytest.raises(KeyError, match=r".*from_asdf expects a file with a 'roman' key"):
+                RomanDataModel.from_asdf(af)
+
+            # Try to open the file with model.from_asdf
+            with pytest.raises(KeyError, match=r".*from_asdf expects a file with a 'roman' key"):
+                model.from_asdf(af)
+
+            # Check if ASDF believes the file is closed (it shouldn't be)
+            #    Note that it has been requested that this be made public in asdf
+            assert af._closed is False
