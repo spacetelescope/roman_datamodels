fe832cc6fd6da3ce98b1cf3424d8e99141c13139
Nadia Dencheva <nadia.dencheva@gmail.com>
2024-01-10 10:12:56 -0500

    add WCS to L2 and L3 models

--- src/roman_datamodels/maker_utils/_datamodels.py
+++ src/roman_datamodels/maker_utils/_datamodels.py
@@ -136,6 +136,8 @@ def mk_level2_image(*, shape=(4088, 4088), n_groups=8, filepath=None, **kwargs):
     )
     wfi_image["cal_logs"] = mk_cal_logs(**kwargs)
 
+    wfi_image["wcs"] = None
+
     return save_node(wfi_image, filepath=filepath)
 
 
@@ -188,6 +190,8 @@ def mk_level3_mosaic(*, shape=(4088, 4088), n_images=2, filepath=None, **kwargs)
         "var_flat", u.Quantity(np.zeros(shape, dtype=np.float32), u.electron**2 / u.s**2, dtype=np.float32)
     )
     wfi_mosaic["cal_logs"] = mk_cal_logs(**kwargs)
+
+    wfi_mosaic["wcs"] = None
 
     return save_node(wfi_mosaic, filepath=filepath)
 
