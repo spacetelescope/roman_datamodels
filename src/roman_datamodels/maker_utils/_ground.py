1e23aecc5e55754f3d66a0f7cc47a3ab1a89e38c
William Jamieson <wjamieson@stsci.edu>
2024-01-30 14:04:31 -0500

    Added makerutil for GroundGroundtest

--- src/roman_datamodels/maker_utils/_ground.py
+++ src/roman_datamodels/maker_utils/_ground.py
@@ -1,3 +1,5 @@
+import astropy.units as u
+import numpy as np
 from astropy import time
 
 from roman_datamodels import stnode
@@ -54,3 +56,39 @@ def mk_ground_guidestar(**kwargs):
     guide["gw_window_ysize"] = kwargs.get("gw_window_ysize", 24)
 
     return guide
+
+
+def mk_ground_groundtest(**kwargs):
+    """
+    Create a dummy GroundGroundtest instance with valid values for attributes
+    required by the schema. Utilized by the model maker utilities below.
+
+    Returns
+    -------
+    roman_datamodels.stnode.GroundGroundtest
+    """
+
+    ground = stnode.GroundGroundtest()
+    ground["test_name"] = kwargs.get("test_name", NOSTR)
+    ground["test_phase"] = kwargs.get("test_phase", NOSTR)
+    ground["test_environment"] = kwargs.get("test_environment", NOSTR)
+    ground["test_script"] = kwargs.get("test_script", NOSTR)
+    ground["product_date"] = kwargs.get("product_date", time.Time("2020-01-01T00:00:00.0", format="isot", scale="utc"))
+    ground["product_version"] = kwargs.get("product_version", NOSTR)
+    ground["conversion_date"] = kwargs.get("conversion_date", time.Time("2020-01-01T00:00:00.0", format="isot", scale="utc"))
+    ground["conversion_version"] = kwargs.get("conversion_version", NOSTR)
+    ground["filename_pnt5"] = kwargs.get("filename_pnt5", NOSTR)
+    ground["filepath_level_pnt5"] = kwargs.get("filepath_level_pnt5", NOSTR)
+    ground["filename_l1a"] = kwargs.get("filename_l1a", NOSTR)
+    ground["detector_id"] = kwargs.get("detector_id", NOSTR)
+    ground["detector_temp"] = kwargs.get("detector_temp", NONUM)
+    ground["frames_temp"] = kwargs.get("frames_temp", np.zeros(6, dtype=np.float32))
+    ground["ota_temp"] = kwargs.get("ota_temp", NONUM)
+    ground["rcs_on"] = kwargs.get("rcs_on", False)
+    ground["readout_col_num"] = kwargs.get("readout_col_num", NONUM)
+    ground["detector_pixel_size"] = kwargs.get(
+        "detector_pixel_size", u.Quantity(np.zeros(6, dtype=np.float32), unit=u.cm, dtype=np.float32)
+    )
+    ground["sensor_error"] = kwargs.get("sensor_error", np.zeros(6, dtype=np.float32))
+
+    return ground
