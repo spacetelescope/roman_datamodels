285cf7d69d64382d7666fc10ba3dc542817030c6
William Jamieson <wjamieson@stsci.edu>
2024-01-30 16:45:44 -0500

    Fix tvac maker utils

--- src/roman_datamodels/maker_utils/_ground.py
+++ src/roman_datamodels/maker_utils/_ground.py
@@ -61,7 +61,7 @@ def mk_ground_guidestar(**kwargs):
     return guide
 
 
-def mk_ground_groundtest(**kwargs):
+def mk_groundtest(**kwargs):
     """
     Create a dummy GroundGroundtest instance with valid values for attributes
     required by the schema. Utilized by the model maker utilities below.
@@ -71,7 +71,7 @@ def mk_ground_groundtest(**kwargs):
     roman_datamodels.stnode.GroundGroundtest
     """
 
-    ground = stnode.GroundGroundtest()
+    ground = stnode.Groundtest()
     ground["test_name"] = kwargs.get("test_name", NOSTR)
     ground["test_phase"] = kwargs.get("test_phase", NOSTR)
     ground["test_environment"] = kwargs.get("test_environment", NOSTR)
@@ -109,7 +109,8 @@ def mk_tvac_groundtest(**kwargs):
     roman_datamodels.stnode.GroundGroundtest
     """
 
-    ground = mk_ground_groundtest(**kwargs)
+    ground = stnode.TvacGroundtest()
+    ground._data = mk_groundtest(**kwargs)._data
 
     ground["activity_number"] = kwargs.get("activity_number", NONUM)
     ground["led_bank1_band_number_on"] = kwargs.get("led_bank1_band_number_on", [NONUM])
@@ -132,7 +133,7 @@ def mk_tvac_groundtest(**kwargs):
     ground["wfi_mce_srcs_bank2_led_range"] = kwargs.get("wfi_mce_srcs_bank2_led_range", NOSTR)
     ground["srcs_led_current"] = kwargs.get("srcs_led_current", NONUM)
     ground["wfi_opt_targettype"] = kwargs.get("wfi_opt_targettype", "FLAT-sRCS")
-    ground["anaysis_tag"] = kwargs.get("anaysis_tag", NOSTR)
+    ground["analysis_tag"] = kwargs.get("analysis_tag", NOSTR)
     ground["gsorc_pose_mode"] = kwargs.get("gsorc_pose_mode", NOSTR)
     ground["gsorc_pose_target"] = kwargs.get("gsorc_pose_target", NOSTR)
     ground["gsorc_sds_active_atten"] = kwargs.get("gsorc_sds_active_atten", NONUM)
@@ -143,7 +144,7 @@ def mk_tvac_groundtest(**kwargs):
     ground["gsorc_sds_dq_pulse"] = kwargs.get("gsorc_sds_dq_pulse", "pulse")
     ground["gsorc_sds_daq_pw"] = kwargs.get("gsorc_sds_daq_pw", u.Quantity(NONUM, u.ms))
     ground["gsorc_heater1_setpt"] = kwargs.get("gsorc_heater1_setpt", NONUM)
-    ground["wfi_opt_wfi_ewa"] = kwargs.get("wfi_opt_wfi_ewa", NOSTR)
+    ground["wfi_otp_wfi_ewa"] = kwargs.get("wfi_otp_wfi_ewa", NOSTR)
     ground["sca_temp"] = kwargs.get("sca_temp", u.Quantity(NONUM, u.K))
     ground["mpa_temp"] = kwargs.get("mpa_temp", u.Quantity(NONUM, u.K))
     ground["ewa_temp"] = kwargs.get("ewa_temp", u.Quantity(NONUM, u.K))
@@ -156,7 +157,7 @@ def mk_tvac_groundtest(**kwargs):
     ground["wfi_sce_1_vbiaspwr_v"] = kwargs.get("wfi_sce_1_vbiaspwr_v", u.Quantity(NONUM, u.V))
     ground["wfi_sce_1_vreset_v"] = kwargs.get("wfi_sce_1_vreset_v", u.Quantity(NONUM, u.V))
     ground["wfi_sce_1_vreset_i"] = kwargs.get("wfi_sce_1_vreset_i", u.Quantity(NONUM, u.A))
-    ground["wfi_mcu_a_offs_csense_fpessen"] = kwargs.get("wfi_mcu_a_offs_csense_fpessen", u.Quantity(NONUM, u.K))
+    ground["wfi_mcu_a_offs_csense_fpssen"] = kwargs.get("wfi_mcu_a_offs_csense_fpssen", u.Quantity(NONUM, u.K))
 
     return ground
 
@@ -176,12 +177,26 @@ def mk_ground_common_meta(**kwargs):
     meta["cal_step"] = mk_cal_step(**kwargs.get("cal_step", {}))
     meta["exposure"] = mk_ground_exposure(**kwargs.get("exposure", {}))
     meta["guidestar"] = mk_ground_guidestar(**kwargs.get("guidestar", {}))
-    meta["groundtest"] = mk_ground_groundtest(**kwargs.get("groundtest", {}))
     meta["instrument"] = mk_wfi_mode(**kwargs.get("instrument", {}))
     meta["ref_file"] = mk_ref_file(**kwargs.get("ref_file", {}))
     meta["hdf5_meta"] = kwargs.get("hdf5_meta", {"test": NOSTR})
     meta["hdf5_telemetry"] = kwargs.get("hdf5_telemetry", NOSTR)
     meta["gw_meta"] = kwargs.get("gw_meta", {"test": NOSTR})
+
+    return meta
+
+
+def mk_fps_meta(**kwargs):
+    """
+    Create a dummy common metadata dictionary with valid values for attributes
+
+    Returns
+    -------
+    dict (defined by the fps-1.0.0.meta schema)
+    """
+
+    meta = mk_ground_common_meta(**kwargs)
+    meta["groundtest"] = mk_groundtest(**kwargs.get("groundtest", {}))
 
     return meta
 
@@ -212,7 +227,7 @@ def mk_fps(*, shape=(8, 4096, 4096), filepath=None, **kwargs):
         warnings.warn("Input shape must be 3D. Defaulting to (8, 4096, 4096)")
 
     fps = stnode.Fps()
-    fps["meta"] = mk_ground_common_meta(**kwargs.get("meta", {}))
+    fps["meta"] = mk_fps_meta(**kwargs.get("meta", {}))
 
     n_groups = shape[0]
 
@@ -283,7 +298,7 @@ def mk_tvac(*, shape=(8, 4096, 4096), filepath=None, **kwargs):
         warnings.warn("Input shape must be 3D. Defaulting to (8, 4096, 4096)")
 
     tvac = stnode.Tvac()
-    tvac["meta"] = mk_ground_common_meta(**kwargs.get("meta", {}))
+    tvac["meta"] = mk_tvac_meta(**kwargs.get("meta", {}))
 
     n_groups = shape[0]
 
