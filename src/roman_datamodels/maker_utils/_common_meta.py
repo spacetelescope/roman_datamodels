c2384774c15763b89a472a08c88db460eb50e64d
William Jamieson <wjamieson@stsci.edu>
2024-02-05 11:47:56 -0500

    Attempt to fix dev

--- src/roman_datamodels/maker_utils/_common_meta.py
+++ src/roman_datamodels/maker_utils/_common_meta.py
@@ -1,6 +1,9 @@
-from astropy import time
+from astropy import coordinates, time
 from astropy import units as u
+from astropy.modeling import models
 from astropy.table import QTable
+from gwcs import coordinate_frames
+from gwcs.wcs import WCS
 
 from roman_datamodels import stnode
 
@@ -773,3 +776,21 @@ def mk_individual_image_meta(**kwargs):
     imm["wcsinfo"] = kwargs.get("wcsinfo", QTable(table_dct))
 
     return imm
+
+
+def mk_wcs():
+    pixelshift = models.Shift(-500) & models.Shift(-500)
+    pixelscale = models.Scale(0.1 / 3600.0) & models.Scale(0.1 / 3600.0)  # 0.1 arcsec/pixel
+    tangent_projection = models.Pix2Sky_TAN()
+    celestial_rotation = models.RotateNative2Celestial(30.0, 45.0, 180.0)
+
+    det2sky = pixelshift | pixelscale | tangent_projection | celestial_rotation
+
+    detector_frame = coordinate_frames.Frame2D(name="detector", axes_names=("x", "y"), unit=(u.pix, u.pix))
+    sky_frame = coordinate_frames.CelestialFrame(reference_frame=coordinates.ICRS(), name="icrs", unit=(u.deg, u.deg))
+    return WCS(
+        [
+            (detector_frame, det2sky),
+            (sky_frame, None),
+        ]
+    )
