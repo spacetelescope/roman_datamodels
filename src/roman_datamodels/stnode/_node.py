fdf1e8849bd88d36c1092d51d45842b8353b2889
Brett <brettgraham@gmail.com>
2025-05-08 17:55:24 -0400

    remove SchemaProperties

--- src/roman_datamodels/stnode/_node.py
+++ src/roman_datamodels/stnode/_node.py
@@ -48,52 +48,6 @@ def _get_schema_for_property(schema, attr):
     return {}
 
 
-class SchemaProperties:
-    """
-    This class provides the capability for using the "contains" machinery
-    so that an attribute can be tested against patternProperties as well
-    as whether the attribute is explicitly a property of the schema.
-    """
-
-    def __init__(self, explicit_properties, patterns):
-        self.explicit_properties = set(explicit_properties)
-        self.patterns = patterns
-
-    def __contains__(self, attr):
-        if attr in self.explicit_properties:
-            return True
-        else:
-            for key in self.patterns.keys():
-                if re.match(key, attr):
-                    return True
-        return False
-
-    def extend(self, other):
-        """
-        Extend the current SchemaProperties with those from another instance.
-        """
-        self.explicit_properties = set(self.explicit_properties).union(other.explicit_properties)
-        self.patterns.update(other.patterns)
-
-    @classmethod
-    def from_schema(cls, schema):
-        """
-        Create a SchemaProperties object from a schema.
-        """
-
-        # Handle the top-level properties
-        explicit_properties = schema.get("properties", {}).keys()
-        patterns = schema.get("patternProperties", {})
-        schema_properties = cls(explicit_properties, patterns)
-
-        # Handle the case where the schema is using an "allOf" combiner
-        if "allOf" in schema:
-            for subschema in schema["allOf"]:
-                schema_properties.extend(cls.from_schema(subschema))
-
-        return schema_properties
-
-
 class DNode(MutableMapping):
     """
     Base class describing all "object" (dict-like) data nodes for STNode classes.
@@ -115,7 +69,6 @@ class DNode(MutableMapping):
         self._schema_uri = None
         self._parent = parent
         self._name = name
-        self._x_schema_attributes = None
 
     @staticmethod
     def _convert_to_scalar(key, value, ref=None):
