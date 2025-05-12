f082511779c42b0f24475b9ccb7f0e7d17525d0a
Brett <brettgraham@gmail.com>
2025-05-12 15:22:51 -0400

    allow from_schema with a model

--- src/roman_datamodels/stnode/_mixins.py
+++ src/roman_datamodels/stnode/_mixins.py
@@ -49,12 +49,14 @@ class WfiModeMixin:
 class FileDateMixin:
     @classmethod
     def from_schema(cls, defaults=None):
-        # TODO handle defaults
+        if defaults:
+            return cls(defaults)
         return cls.now()
 
     @classmethod
     def fake_data(cls, defaults=None):
-        # TODO handle defaults
+        if defaults:
+            return cls(defaults)
         return cls("2020-01-01T00:00:00.0", format="isot", scale="utc")
 
 
@@ -69,38 +71,43 @@ class TvacFileDateMixin(FileDateMixin):
 class CalibrationSoftwareNameMixin:
     @classmethod
     def from_schema(cls, defaults=None):
-        # TODO handle defaults
+        if defaults:
+            return cls(defaults)
         return cls("RomanCAL")
 
 
 class OriginMixin:
     @classmethod
     def from_schema(cls, defaults=None):
-        # TODO handle defaults
+        if defaults:
+            return cls(defaults)
         return cls("STSCI")
 
 
 class TelescopeMixin:
     @classmethod
     def from_schema(cls, defaults=None):
-        # TODO handle defaults
+        if defaults:
+            return cls(defaults)
         return cls("ROMAN")
 
 
 class RefFileMixin:
     @classmethod
     def from_schema(cls, defaults=None):
-        # TODO handle defaults
+        defaults = defaults or {}
+        # TODO the nested "crds" dict won't be copied here
         schema = _get_schema_from_tag(cls._default_tag)
-        return cls({k: "NA" for k, v in schema["properties"].items() if v["type"] == "string"})
+        keys = defaults.keys() | {k for k, v in schema["properties"].items() if v["type"] == "string"}
+        return cls({k: defaults.get(k, "NA") for k in keys})
 
 
 class L2CalStepMixin:
     @classmethod
     def from_schema(cls, defaults=None):
-        # TODO handle defaults
+        defaults = defaults or {}
         schema = _get_schema_from_tag(cls._default_tag)
-        return cls({k: "INCOMPLETE" for k in schema["properties"]})
+        return cls({k: defaults.get(k, "INCOMPLETE") for k in schema["properties"]})
 
 
 class L3CalStepMixin(L2CalStepMixin):  # same as L2CalStepMixin
