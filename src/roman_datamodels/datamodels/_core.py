6f3382e4e5f0323517efaf176788df75730fb43a
William Jamieson <william@jamieson.phd>
2025-07-11 11:24:24 -0400

    Fix import of private functions from modules in datamodels subpackage

--- src/roman_datamodels/datamodels/_core.py
+++ src/roman_datamodels/datamodels/_core.py
@@ -13,7 +13,6 @@ import copy
 import datetime
 import functools
 import sys
-from contextlib import contextmanager
 from pathlib import Path, PurePath
 
 import asdf
@@ -50,26 +49,6 @@ def _set_default_asdf(func):
         return func(self, *args, **kwargs)
 
     return wrapper
-
-
-@contextmanager
-def _temporary_update_filename(datamodel, filename):
-    """
-    Context manager to temporarily update the filename of a datamodel so that it
-    can be saved with that new file name without changing the current model's filename
-    """
-    from roman_datamodels.stnode import Filename
-
-    if "meta" in datamodel._instance and "filename" in datamodel._instance.meta:
-        old_filename = datamodel._instance.meta.filename
-        datamodel._instance.meta.filename = Filename(filename)
-
-        yield
-        datamodel._instance.meta.filename = old_filename
-        return
-
-    yield
-    return
 
 
 class DataModel(abc.ABC):
@@ -299,8 +278,10 @@ class DataModel(abc.ABC):
             return asdf.AsdfFile(init, **kwargs)
 
     def to_asdf(self, init, *args, **kwargs):
+        from ._utils import temporary_update_filename
+
         all_array_compression = kwargs.pop("all_array_compression", "lz4")
-        with validate.nuke_validation(), _temporary_update_filename(self, Path(init).name):
+        with validate.nuke_validation(), temporary_update_filename(self, Path(init).name):
             asdf_file = self.open_asdf(**kwargs)
             asdf_file["roman"] = self._instance
             asdf_file.write_to(init, *args, all_array_compression=all_array_compression, **kwargs)
