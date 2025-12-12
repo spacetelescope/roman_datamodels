390a3d399c21db278c04ef22aa4ded8df7ab8972
William Jamieson <william@jamieson.phd>
2025-12-12 15:06:28 -0500

    Add public api for rcal

--- src/roman_datamodels/__init__.py
+++ src/roman_datamodels/__init__.py
@@ -1,4 +1,5 @@
+from ._stnode import get_latest_schema
 from ._version import version as __version__
 from .datamodels import DataModel, open
 
-__all__ = ["DataModel", "__version__", "open"]
+__all__ = ["DataModel", "__version__", "get_latest_schema", "open"]
