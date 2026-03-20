da0ee32cef286863f357277fd6d3cde68518fa28
Brett Graham <brettgraham@gmail.com>
2026-03-20 15:21:16 -0400

    add test that checks l2 files use lz4 compression (#636)

---
+++ tests/test_moc_expectations.py
@@ -0,0 +1,26 @@
+"""
+These tests are to confirm that L2 files conform to expectations
+of the mission operations center (MOC). Additional expectations
+are covered in RAD tests (which is the preferred way to check that
+files conform to expectations). Any changes that impact these
+expectations must be coordinated with the MOC.
+"""
+
+import asdf
+
+from roman_datamodels.datamodels import ImageModel
+
+
+def test_l2_compression(tmp_path):
+    """
+    Test that L2 files use lz4 compression.
+    """
+    fn = tmp_path / "test.asdf"
+    ImageModel.create_fake_data().save(fn)
+
+    compression_codes = set()
+    with asdf.open(fn) as af:
+        for node in af.search(type_=asdf.tags.core.NDArrayType).nodes:
+            compression_codes.add(af.get_array_compression(node))
+
+    assert compression_codes == {"lz4"}
