9c2f220ff927cdd1feec992a2d6a06e3e93cb3b4
Brett Graham <brettgraham@gmail.com>
2026-04-06 19:27:41 -0400

    remove deprecated stnode (#646)
    Co-authored-by: Paul Huwe <42071634+PaulHuwe@users.noreply.github.com>

--- src/roman_datamodels/table_definitions.py
+++
@@ -1,8 +0,0 @@
-# Pleaceholder for future tables, ex:
-#
-# DQ_DEF_DTYPE = np.dtype([
-#     ("BIT", np.int32),
-#     ("VALUE", np.uint32),
-#     ("NAME", "S40"),
-#     ("DESCRIPTION", "S80")
-# ])
